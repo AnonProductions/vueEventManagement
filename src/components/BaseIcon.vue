cd0a7b899e79c0eeedeaef410ca6c2a2f3a4d6d6
AnonProductions <46707105+AnonProductions@users.noreply.github.com>
2020-08-14 02:35:03 -0700

    Add files via upload

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
+    </svg>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.icon-wrapper {
+  display: inline-flex;
+
+  span {
+    margin-left: 10px;
+  }
+}
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <BaseIcon />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
+
+export default {
+  data() {
+    return {
+      events: [],
+    };
+  },
+  components: {
+    EventCard,
+  },
+  created() {
+    EventService.getEvents()
+      .then((res) => (this.events = res.data))
+      .catch((err) => console.log(`Error => ${err.response}`));
+  },
+};
+</script>
